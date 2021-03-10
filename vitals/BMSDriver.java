0e4612b13e34fc562bb2cb9ef6b5da914179ddcd
ARULPRIYAM30 <77484841+ARULPRIYAM30@users.noreply.github.com>
2021-03-10 13:49:52 +0530

    Update BMSDriver.java

--- vitals/BMSDriver.java
+++ vitals/BMSDriver.java
@@ -11,14 +11,14 @@ public class BMSDriver {
     }
 	
     public static void main(String[] args) {
-    	
     	BatteryLimit estimateLimit=new BatteryLimit();
     	assert(SwitchLanguage.setLanguageInSystem("EN")==true);
-    	assert(estimateLimit.setTemperatureUnit("Fahrenheit")==true);
-    	assert(batteryTest(140, 60, 0.6f) == false);
-    	assert(batteryTest(25, 70, 0.7f) == true);
+    	assert(batteryTest(30, 70, 0.7f) == true);
         assert(batteryTest(-1, -20, 0.7f)==false);
-    	assert(batteryTest(44, 79, 0.7f)==true);
+    	assert(batteryTest(34, 79, 0.7f)==true);
+	assert(batteryTest(43, 69, 0.7f)==true);
+	assert(estimateLimit.setTemperatureUnit("Fahrenheit")==true);
+    	assert(batteryTest(140, 60, 0.6f) == false);
     	
     }
 }
