9510e65986bb0160eae4a10e9dffc459adff0f7e
ARULPRIYAM30 <77484841+ARULPRIYAM30@users.noreply.github.com>
2021-03-15 08:32:39 +0530

    Update BatteryConstants.java

--- vitals/BatteryConstants.java
+++ vitals/BatteryConstants.java
@@ -1,10 +1,9 @@
 package vitals;
 
 public class BatteryConstants {
-	
-	
+
 	public float value, maxLimit, minimumLimit, lowBreach, highBreach;
-    public String parameter;
+    	public String parameter;
     
 	static final float MAX_CHARGE_RATE= 0.8f;
 	static final float MIN_CHARGE_RATE= 0.0f;
@@ -19,12 +18,10 @@ public class BatteryConstants {
 	static final String LOW_BREACH_WARNING_ENG="is about to approach low breach";
 
 	static final String	LOW_THRESHOLD_DE="liegt unter dem Schwellenwert";
-	static final String HIGH_THRESHOLD_DE="liegt über dem Schwellenwert";
-	static final String	HIGH_BREACH_WARNING_DE="ist kurz davor, sich einer hohen Verletzung zu nähern";
-	static final String LOW_BREACH_WARNING_DE="ist im Begriff, sich einer geringen Verletzung zu nähern";
+	static final String HIGH_THRESHOLD_DE="liegt uber dem Schwellenwert";
+	static final String	HIGH_BREACH_WARNING_DE="ist kurz davor, sich einer hohen Verletzung zu nahern";
+	static final String LOW_BREACH_WARNING_DE="ist im Begriff, sich einer geringen Verletzung zu nahern";
 	static final String BATTERY_OK="Battery Status OK";
-
-	
     
     public BatteryConstants(String battery_parameter, float value, float maxLimit, float minimumLimit)
     {
